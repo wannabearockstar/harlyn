073128bf8b74b5b764c9ec9e7a089377c7a5ada4
wannabe <smelov.kirill@gmail.com>
2015-11-10 21:47:35 +1000

    Init spring

---
+++ src/main/java/com/harlyn/HarlynApplication.java
@@ -0,0 +1,12 @@
+package com.harlyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarlynApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarlynApplication.class, args);
+    }
+}
