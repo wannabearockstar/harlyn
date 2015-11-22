df8abdd5f197709c10db6ffa131ab4f91ae9f3e6
wannabe <smelov.kirill@gmail.com>
2015-11-22 17:37:40 +1000

    Refactor: move admin actions to separate controllers

--- src/main/java/com/harlyn/web/admin/AdminProblemController.java
+++ src/main/java/com/harlyn/web/admin/AdminProblemController.java
@@ -1,12 +1,9 @@
-package com.harlyn.web;
+package com.harlyn.web.admin;
 
 import com.harlyn.domain.problems.Problem;
 import com.harlyn.domain.problems.handlers.ProblemHandler;
 import com.harlyn.exception.ProblemNotFoundException;
 import com.harlyn.service.ProblemService;
-import com.harlyn.service.SolutionService;
-import com.harlyn.service.TeamService;
-import com.harlyn.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,29 +16,23 @@ import javax.annotation.Resource;
 import java.util.Map;
 
 /**
- * Created by wannabe on 21.11.15.
+ * Created by wannabe on 22.11.15.
  */
 @Controller
-@RequestMapping(value = "/admin")
-public class AdminController {
+@RequestMapping(value = "/admin/problem")
+public class AdminProblemController {
     @Autowired
     private ProblemService problemService;
     @Resource
     private Map<Problem.ProblemType, ProblemHandler> problemHandlers;
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private TeamService teamService;
-    @Autowired
-    private SolutionService solutionService;
 
-    @RequestMapping(value = "/problem/", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String newProblemPage(Model model) {
         model.addAttribute("problem_handlers_keys", problemHandlers.keySet());
         return "admin/problem/new";
     }
 
-    @RequestMapping(value = "/problem/", method = RequestMethod.POST)
+    @RequestMapping(value = "/", method = RequestMethod.POST)
     public String newProblemAction(
             @RequestParam(value = "name") String name,
             @RequestParam(value = "answer") String answer,
@@ -55,7 +46,7 @@ public class AdminController {
         );
     }
 
-    @RequestMapping(value = "/problem/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public String editProblemPage(@PathVariable(value = "id") Long id, Model model) {
         Problem problem = problemService.getById(id);
         if (problem == null) {
@@ -66,7 +57,7 @@ public class AdminController {
         return "admin/problem/edit";
     }
 
-    @RequestMapping(value = "/problem/{id}", method = RequestMethod.POST)
+    @RequestMapping(value = "/{id}", method = RequestMethod.POST)
     public String editProblemAction(
             @PathVariable(value = "id") Long id,
             @RequestParam(value = "name") String name,
@@ -86,27 +77,9 @@ public class AdminController {
         );
     }
 
-    @RequestMapping(value = "/problem/list", method = RequestMethod.GET)
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String listProblemPage(Model model) {
         model.addAttribute("problems", problemService.getAllProblems());
         return "admin/problem/list";
-    }
-
-    @RequestMapping(value = "/user/list", method = RequestMethod.GET)
-    public String listUsersPage(Model model) {
-        model.addAttribute("users", userService.getAllUsers());
-        return "admin/user/list";
-    }
-
-    @RequestMapping(value = "/team/list", method = RequestMethod.GET)
-    public String listTeamPage(Model model) {
-        model.addAttribute("teams", teamService.getAllTeams());
-        return "admin/team/list";
-    }
-
-    @RequestMapping(value = "/solution/list", method = RequestMethod.GET)
-    public String listSolutionPage(Model model) {
-        model.addAttribute("solutions", solutionService.getAllSolutions());
-        return "admin/solution/list";
     }
 }
---
+++ src/main/java/com/harlyn/web/admin/AdminUserController.java
@@ -0,0 +1,24 @@
+package com.harlyn.web.admin;
+
+import com.harlyn.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by wannabe on 22.11.15.
+ */
+@Controller
+@RequestMapping(value = "/admin/user")
+public class AdminUserController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String listUsersPage(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "admin/user/list";
+    }
+}
