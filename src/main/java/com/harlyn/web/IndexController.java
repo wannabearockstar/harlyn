06eec85a76335e965c5f9f9b6a53f642d51f4849
wannabe <smelov.kirill@gmail.com>
2015-11-26 15:59:14 +1000

    Add index page

---
+++ src/main/java/com/harlyn/web/IndexController.java
@@ -0,0 +1,16 @@
+package com.harlyn.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by wannabe on 26.11.15.
+ */
+@Controller
+public class IndexController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String indexPage() {
+        return "redirect:/problem/";
+    }
+}
