6a8093baeedce90e72baf0f1e1eaa39a7405c6b6
wannabe <smelov.kirill@gmail.com>
2015-12-03 13:40:28 +1000

    Add default message for not registerd team exception

--- src/main/java/com/harlyn/exception/TeamNotRegisteredForCompetitionException.java
+++ src/main/java/com/harlyn/exception/TeamNotRegisteredForCompetitionException.java
@@ -4,4 +4,7 @@ package com.harlyn.exception;
  * Created by wannabe on 02.12.15.
  */
 public class TeamNotRegisteredForCompetitionException extends RuntimeException {
+    public TeamNotRegisteredForCompetitionException() {
+        super("Team not register for corresponding competition");
+    }
 }
