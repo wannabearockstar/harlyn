1ae3a4be894c896a81f3a7cb29a4737b4c8fc4f6
wannabe <smelov.kirill@gmail.com>
2015-12-03 12:11:17 +1000

    Change competitionNotFOund exception param signature

--- src/main/java/com/harlyn/exception/CompetitionNotFoundException.java
+++ src/main/java/com/harlyn/exception/CompetitionNotFoundException.java
@@ -4,7 +4,7 @@ package com.harlyn.exception;
  * Created by wannabe on 02.12.15.
  */
 public class CompetitionNotFoundException extends RuntimeException {
-    public CompetitionNotFoundException(Long teamId) {
-        super("Competition with id " + teamId + " not found");
+    public CompetitionNotFoundException(Long competitionId) {
+        super("Competition with id " + competitionId + " not found");
     }
 }
