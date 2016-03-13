0397956331f885d37417246ca9734db556665cbc
wannabe <smelov.kirill@gmail.com>
2016-03-13 17:08:26 +1000

    Competition problems filtering

--- src/main/java/com/harlyn/web/CompetitionController.java
+++ src/main/java/com/harlyn/web/CompetitionController.java
@@ -5,13 +5,16 @@ import com.harlyn.domain.competitions.Competition;
 import com.harlyn.exception.CompetitionNotFoundException;
 import com.harlyn.exception.OutdatedCompetitionException;
 import com.harlyn.exception.TeamNotRegisteredForCompetitionException;
+import com.harlyn.service.CategoryService;
 import com.harlyn.service.CompetitionService;
+import org.apache.commons.lang.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Date;
 
@@ -24,6 +27,8 @@ public class CompetitionController {
 
 	@Autowired
 	private CompetitionService competitionService;
+	@Autowired
+	private CategoryService categoryService;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public String competitionPage(@PathVariable(value = "id") Long id, Model model) {
@@ -62,7 +67,9 @@ public class CompetitionController {
 	}
 
 	@RequestMapping(value = "/{id}/problems", method = RequestMethod.GET)
-	public String problemsPage(@PathVariable(value = "id") Long id, Model model) {
+	public String problemsPage(@PathVariable(value = "id") Long id, Model model,
+														 @RequestParam(value = "category", required = false, defaultValue = "0") String categoryId
+	) {
 		Competition competition = competitionService.findById(id);
 		if (competition == null) {
 			throw new CompetitionNotFoundException(id);
@@ -70,7 +77,17 @@ public class CompetitionController {
 		if (!competitionService.isTeamRegistered(competition, ((User) model.asMap().get("me")).getTeam())) {
 			throw new TeamNotRegisteredForCompetitionException();
 		}
+		//// TODO: 13.03.16 custom mapper
+		Long categoryIdParsed;
+		if (!NumberUtils.isNumber(categoryId)) {
+			categoryIdParsed = 0L;
+		} else {
+			categoryIdParsed = NumberUtils.toLong(categoryId);
+		}
+
 		model.addAttribute("competition", competition);
+		model.addAttribute("selected_category_id", categoryIdParsed);
+		model.addAttribute("categories", categoryService.findAll());
 		return "competition/problems";
 	}
 
